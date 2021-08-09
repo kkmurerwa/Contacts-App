47622313c122c399598870c04cab46942e74ae68
kanake10 <10kanake@gmail.com>
2021-08-09 15:45:03 +0300

    entity

---
+++ app/src/main/java/com/example/mycontacts/data/Dao/Contact.kt
@@ -0,0 +1,13 @@
+package com.example.mycontacts.data.Dao
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "contacts_table")
+data class Contact(
+    @PrimaryKey(autoGenerate =true)
+    val id:Int,
+    val name:String,
+    val email:String,
+    val number:Int
+)
