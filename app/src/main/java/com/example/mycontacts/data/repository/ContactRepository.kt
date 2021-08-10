198d91bd7d1de859cba5c4cc31dcf18b63b3a390
kanake10 <10kanake@gmail.com>
2021-08-10 12:53:01 +0300

    saving changes

--- app/src/main/java/com/example/mycontacts/data/repository/ContactRepository.kt
+++ app/src/main/java/com/example/mycontacts/data/repository/ContactRepository.kt
@@ -1,9 +1,10 @@
 package com.example.mycontacts.data.repository
 
+import androidx.lifecycle.LiveData
 import com.example.mycontacts.data.Entity.Contact
 
 class ContactRepository(private val contactDao:ContactDao) {
-    val readAllData:LiveData<List<Contact>> =ContactDao.readAllData()
+    val readAllData: LiveData<List<Contact>> =ContactDao.readAllData()
 
     suspend fun addContact(contact:Contact){
         contactDao.addContact(contact)
