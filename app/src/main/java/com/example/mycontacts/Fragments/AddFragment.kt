dcd896cf4b1ee76a31416bab619d15ed6822861f
kanake10 <10kanake@gmail.com>
2021-08-14 02:41:21 +0300

    inserting data

--- app/src/main/java/com/example/mycontacts/Fragments/AddFragment.kt
+++ app/src/main/java/com/example/mycontacts/Fragments/AddFragment.kt
@@ -5,16 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.mycontacts.R
+import com.example.mycontacts.data.ContactViewModel
+import kotlinx.android.synthetic.main.fragment_add.view.*
 
 class AddFragment : Fragment() {
 
+    private lateinit var myContactViewModel:ContactViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        val view= inflater.inflate(R.layout.fragment_add, container, false)
 
+        myContactViewModel =ViewModelProvider(this).get(ContactViewModel::class.java)
+
+    view.savebutton.setOnClickListener{
+        insertData()
+
+    }
+
+        return view
+    }
+
+    private fun insertData() {
+        TODO("Not yet implemented")
     }
 }
