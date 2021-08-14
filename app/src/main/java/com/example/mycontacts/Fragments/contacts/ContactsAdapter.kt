9ec669469be99c2a9e3af10fcf296ff56dbe85d9
kanake10 <10kanake@gmail.com>
2021-08-14 04:57:22 +0300

    adapter

--- app/src/main/java/com/example/mycontacts/Fragments/contacts/ContactsAdapter.kt
+++ app/src/main/java/com/example/mycontacts/Fragments/contacts/ContactsAdapter.kt
@@ -1,4 +1,42 @@
 package com.example.mycontacts.Fragments.contacts
 
-class ContactsAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mycontacts.R
+import com.example.mycontacts.data.Entity.Contact
+import kotlinx.android.synthetic.main.my_rows.view.*
+
+class ContactsAdapter: RecyclerView.Adapter<ContactsAdapter.MyViewHolder>() {
+
+    private var contactList= emptyList<Contact>()
+
+    class MyViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+
+
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.my_rows,parent,false))
+    }
+
+    override fun getItemCount(): Int {
+        return contactList.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        val currentData=contactList[position]
+        holder.itemView.textView_number.text=currentData.id.toString()
+        holder.itemView.contactsaved_as.text=currentData.name
+        holder.itemView.save_email_address.text=currentData.email
+        holder.itemView.save_contact_as.text=currentData.number.toString()
+    }
+
+    fun setDtails(contact: List<Contact>){
+        this.contactList=contact
+        notifyDataSetChanged()
+    }
+
 }
