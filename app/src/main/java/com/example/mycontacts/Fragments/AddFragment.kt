0f3a7f25f4cd91440c362ef03d749e207baec5c5
kanake10 <10kanake@gmail.com>
2021-08-14 03:05:35 +0300

    addition

--- app/src/main/java/com/example/mycontacts/Fragments/AddFragment.kt
+++ app/src/main/java/com/example/mycontacts/Fragments/AddFragment.kt
@@ -1,14 +1,20 @@
 package com.example.mycontacts.Fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.mycontacts.R
 import com.example.mycontacts.data.ContactViewModel
+import com.example.mycontacts.data.Entity.Contact
+import kotlinx.android.synthetic.main.fragment_add.*
 import kotlinx.android.synthetic.main.fragment_add.view.*
 
 class AddFragment : Fragment() {
@@ -32,6 +38,28 @@ class AddFragment : Fragment() {
     }
 
     private fun insertData() {
-        TODO("Not yet implemented")
+        val name =ContactName.text.toString()
+        val email=EmailAdress.text.toString()
+        val number=PhoneNumber.text
+
+        if(checkDetails(name,email, number as Editable)){
+            val contact = Contact(0,name,email,Integer.parseInt(number.toString()))
+
+            myContactViewModel.addContact(contact)
+            Toast.makeText(requireContext(),"Contact Saved Successfully",Toast.LENGTH_LONG).show()
+
+            findNavController().navigate(R.id.action_addFragment_to_contactsFragment)
+        }else{
+            Toast.makeText(requireContext(),"Fill All Details",Toast.LENGTH_LONG).show()
+        }
+
+
+
+
     }
+
+    private fun checkDetails(name:String,email:String,number:Editable):Boolean{
+        return !(TextUtils.isEmpty(name) && TextUtils.isEmpty(email) && number.isEmpty())
+    }
+
 }
