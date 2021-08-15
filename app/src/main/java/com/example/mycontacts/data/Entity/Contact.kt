5ca54ac58912ef6d27e06acfec5e59e99608b654
kanake10 <10kanake@gmail.com>
2021-08-16 00:46:42 +0300

    bug

--- app/src/main/java/com/example/mycontacts/data/Entity/Contact.kt
+++ app/src/main/java/com/example/mycontacts/data/Entity/Contact.kt
@@ -1,8 +1,12 @@
 package com.example.mycontacts.data.Entity
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 
+
+@Parcelize
 @Entity(tableName = "contacts_table")
 data class Contact(
     @PrimaryKey(autoGenerate =true)
@@ -10,4 +14,4 @@ data class Contact(
     val name:String,
     val email:String,
     val number:Int
-)
+):Parcelable
